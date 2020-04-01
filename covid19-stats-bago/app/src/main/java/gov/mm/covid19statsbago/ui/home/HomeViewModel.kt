95bc7f84e9f7fc507b2208f2fa2b7a33905b110a
waiyanphyoe <waiyanphyoecu.wyp@g,ail.com>
2020-04-01 22:06:40 +0630

    Init project

---
+++ covid19-stats-bago/app/src/main/java/gov/mm/covid19statsbago/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package gov.mm.covid19statsbago.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
