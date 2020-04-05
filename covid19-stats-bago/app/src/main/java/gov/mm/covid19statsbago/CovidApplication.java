1a0af8e4fe82760e96fccb93bfc516b3984fce5b
waiyanphyoe <waiyanphyoecu.wyp@g,ail.com>
2020-04-05 20:10:48 +0630

    added keystore and crashanalytic

---
+++ covid19-stats-bago/app/src/main/java/gov/mm/covid19statsbago/CovidApplication.java
@@ -0,0 +1,17 @@
+package gov.mm.covid19statsbago;
+
+import androidx.multidex.MultiDexApplication;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+/**
+ * Created by Wai Yan on 4/5/20.
+ */
+public class CovidApplication extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseAnalytics.getInstance(this);
+    }
+}
